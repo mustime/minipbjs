79de2a3fee2727786f2df6b8874e3acf54717af8
Irvin Pang <halo.irvin@gmail.com>
2021-05-18 22:25:42 +0800

    release v0.2.0

--- dist/version.js
+++ dist/version.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.VERSION_STR = exports.VERSION_PATCH = exports.VERSION_MINOR = exports.VERSION_MAJOR = void 0;
 exports.VERSION_MAJOR = 0;
-exports.VERSION_MINOR = 1;
-exports.VERSION_PATCH = 2;
+exports.VERSION_MINOR = 2;
+exports.VERSION_PATCH = 0;
 exports.VERSION_STR = exports.VERSION_MAJOR + "." + exports.VERSION_MINOR + "." + exports.VERSION_PATCH;
 //# sourceMappingURL=version.js.map
